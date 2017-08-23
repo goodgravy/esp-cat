7d292f36c477e4ff3532b4c45a09bf8ea09cd990
goodgravy <james@jmsbrdy.com>
2017-08-23 15:55:19 +0200

    feat: add some more translators

--- lib/translate.ex
+++ lib/translate.ex
@@ -14,10 +14,18 @@ defmodule Translate do
   defp step_funcs, do: Keyword.values(steps())
 
   defp steps do
-    [ue_to_o: &ue_to_o/1]
+    [
+      ue_to_o: &ue_to_o/1,
+      ie_to_e: &ie_to_e/1,
+      ion_to_io: &ion_to_io/1,
+      drop_o: &drop_o/1,
+      drop_e: &drop_e/1,
+    ]
   end
 
-  defp ue_to_o(word) do
-    String.replace(word, "ue", "o")
-  end
+  defp ue_to_o(word), do: String.replace(word, "ue", "o")
+  defp ie_to_e(word), do: String.replace(word, "ie", "e")
+  defp ion_to_io(word), do: String.replace(word, ~r/ión$/, "ió")
+  defp drop_o(word), do: String.replace(word, ~r/o$/, "")
+  defp drop_e(word), do: String.replace(word, ~r/e$/, "")
 end
